86c8bdf8472074dbab4810ea7696c286a4060470
OVERWATCHER\Overwatcher <paoloftuna@googmail.com>
2017-12-18 15:25:40 +0000

    +skybox +skybox rot

---
+++ Unity_Project/Assets/Scripts/SkyboxRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    public float RotPerSec = 1;
+    public bool rot = true;
+
+    protected void Update()
+    {
+        if (!rot)
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotPerSec);
+    }
+}
