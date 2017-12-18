f7df4a6f9cfee573efd17929a8124236c0ea188d
OVERWATCHER\Overwatcher <paoloftuna@googmail.com>
2017-12-18 17:09:48 +0000

    +target camera

---
+++ Unity_Project/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour {
+
+    public List<Transform> targets;
+
+    public Vector3 offset;
+
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        Vector3 center = GetCenter();
+        transform.position = center + offset;
+    }
+
+    Vector3 GetCenter()
+    {
+        //if only one then that is the center
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        //when multiple, find the center
+        else
+        {
+            var bounds = new Bounds(targets[0].position, Vector3.zero);
+            for (int i = 0; i < targets.Count; i++)
+            {
+                bounds.Encapsulate(targets[i].position);
+            }
+            return bounds.center;
+        }
+    }
+}
