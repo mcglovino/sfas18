a74d84c7ac5069f4fd66d1757ce3a4cd641a19bb
OVERWATCHER\Overwatcher <paoloftuna@googmail.com>
2017-12-22 16:28:38 +0000

    +you changes colour in WIN scene

--- Unity_Project/Assets/Scripts/PlayerLoad.cs
+++ Unity_Project/Assets/Scripts/PlayerLoad.cs
@@ -7,17 +7,27 @@ public class PlayerLoad : MonoBehaviour {
     public GameObject P1;
     public GameObject P2;
     public GameObject Camera;
+    public GameObject You;
 
 	void Start () {
+
         if (SceneInfo.Win == 1)
         {
             P1.SetActive(true);
             Camera.GetComponent<CameraTarget>().targets.Remove(P2.transform);
+            foreach(Renderer R in You.GetComponentsInChildren<Renderer>())
+            {
+                R.material = P1.GetComponent<Renderer>().material;
+            }
         }
         else
         {
             P2.SetActive(true);
             Camera.GetComponent<CameraTarget>().targets.Remove(P1.transform);
+            foreach (Renderer R in You.GetComponentsInChildren<Renderer>())
+            {
+                R.material = P2.GetComponent<Renderer>().material;
+            }
         }
 	}
 	
