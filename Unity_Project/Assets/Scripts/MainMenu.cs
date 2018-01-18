b5296ad8ff780fd68551d01dd3543ec4f3c2ff91
OVERWATCHER\Overwatcher <paoloftuna@googmail.com>
2018-01-18 17:18:09 +0000

    +quality options added

--- Unity_Project/Assets/Scripts/MainMenu.cs
+++ Unity_Project/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public GameObject optionMenu;
+    public GameObject camera;
+    public GameObject camera2;
+
     public void SinglePlayer()
     {
         SceneManager.LoadScene(1);
@@ -17,12 +21,30 @@ public class MainMenu : MonoBehaviour {
 
     public void Options()
     {
-        SceneManager.LoadScene(1);
+        if(optionMenu.activeSelf == false)
+            optionMenu.SetActive(true);
+        else
+            optionMenu.SetActive(false);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Quality(int qual)
+    {
+        QualitySettings.SetQualityLevel(qual, true);
+        if (qual == 0 || qual == 1 || qual == 2)
+        {
+            camera2.SetActive(true);
+            camera.SetActive(false);
+        }
+        else
+        {
+            camera.SetActive(true);
+            camera2.SetActive(false);
+        }
     }
 
 }
