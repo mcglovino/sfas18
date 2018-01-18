e38ae6e453b5c69514848dd0c7bf4669dff26101
OVERWATCHER\Overwatcher <paoloftuna@googmail.com>
2018-01-18 17:39:35 +0000

    +colour pick scene //not yet implimented into game

---
+++ Unity_Project/Assets/Scripts/ColourChange.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour {
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<MeshRenderer>().material = this.gameObject.GetComponent<MeshRenderer>().material;
+    }
+}
